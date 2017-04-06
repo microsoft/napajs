b3770fc4bb0c65af0296621c4ea1e634093b3413
Daiyi Peng <dapeng@microsoft.com>
2017-04-06 21:51:33 +0000

    Merged PR 231299: Rename @napajs/vineyard to winery
    Rename @napajs/vineyard to winery

--- modules/@napajs/vineyard/lib/object-model.ts
+++
@@ -1,6 +0,0 @@
-// Facade for exporting object-model interfaces and classes.
-
-export * from './object-type';
-export * from './object-provider';
-export * from './named-object';
-export * from './object-context';
