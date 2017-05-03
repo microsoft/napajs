09fd4e4fa1ba41843662069afcf7a83d828e4cea
Asi Bross <asib@microsoft.com>
2017-05-03 23:23:50 +0000

    Merged PR 255847: Zone interface changes
    Zone interface changes

--- modules/napajs/lib/runtime/napa-zone.ts
+++ modules/napajs/lib/runtime/napa-zone.ts
@@ -11,6 +11,34 @@ interface ExecuteRequest {
     transportContext: transport.TransportContext;
 }
 
+export class NapaExecuteResult implements zone.ExecuteResult{
+     
+     constructor(payload: string, transportContext: transport.TransportContext) {
+          this._payload = payload;
+          this._transportContext = transportContext; 
+     }
+
+     get value(): any {
+         if (this._value == null) {
+             this._value = transport.unmarshall(this._payload, this._transportContext);
+         }
+
+         return this._value;
+     }
+
+     get payload(): string {
+         return this._payload; 
+     }
+
+     get transportContext(): transport.TransportContext {
+         return this._transportContext; 
+     }
+
+     private _transportContext: transport.TransportContext;
+     private _payload: string;
+     private _value: any;
+};
+
 /// <summary> Zone consists of Napa isolates. </summary>
 export class NapaZone implements zone.Zone {
     private _nativeZone;
@@ -23,11 +51,31 @@ export class NapaZone implements zone.Zone {
         return this._nativeZone.getId();
     }
 
-    public broadcast(source: string) : Promise<zone.ResponseCode> {
-        if (typeof __in_napa !== 'undefined') {
-            // Napa does not support async yet, we wrap this sync call in a promise
-            // to provide a uniform API.
-            return Promise.resolve(this._nativeZone.broadcastSync(source));
+    public broadcast(arg1: any, arg2?: any) : Promise<zone.ResponseCode> {
+        let source: string;
+        if (typeof arg1 === "string") {
+            // broadcast with source
+            source = arg1;
+        } else {
+            // broadcast with function
+            if (!(arg1 instanceof Function)) {
+                throw new TypeError("Expected a Function type argument");
+            }
+            
+            let functionString: string = (<Function>arg1).toString();
+            
+            // Prepare arguments as a string
+            let argumentsString: string = "";
+            if (arg2 != undefined) {
+                if (!(arg2 instanceof Array)) {
+                    throw new TypeError("Expected an Array type argument");
+                }
+                argumentsString = JSON.stringify(arg2);
+                argumentsString = argumentsString.substring(1, argumentsString.length - 1);
+            }
+
+            // Create a self invoking function string
+            source = `(${ functionString })(${ argumentsString })`;
         }
 
         return new Promise<zone.ResponseCode>(resolve => {
@@ -35,37 +83,29 @@ export class NapaZone implements zone.Zone {
         });
     }
 
-    public execute(module: string, func: string, args: any[], timeout?: number) : Promise<zone.ResponseValue> {        
+    public execute(arg1: any, arg2: any, arg3?: any, arg4?: any) : Promise<zone.ExecuteResult> {
+        if (arg1 instanceof Function) {
+            throw new Error("Execute with function is not implemented");
+        }
+
         let transportContext: transport.TransportContext = transport.createTransportContext();
         let request : ExecuteRequest = {
-            module: module,
-            function: func,
-            arguments: args.map(arg => { return transport.marshall(arg, transportContext); }),
-            timeout: timeout,
+            module: arg1,
+            function: arg2,
+            arguments: (<Array<any>>arg3).map(arg => { return transport.marshall(arg, transportContext); }),
+            timeout: arg4,
             transportContext: transportContext
         };
         
-        if (typeof __in_napa !== 'undefined') {
-            // Napa does not support async yet, we wrap this sync call in a promise
-            // to provide a uniform API.
-            let response = this._nativeZone.executeSync(request);
-            if (response.code === 0) {
-                transportContext = transport.createTransportContext(response.contextHandle);
-                return Promise.resolve(transport.unmarshall(response.returnValue, transportContext));
-            } else {
-                return Promise.reject(response.errorMessage);
-            }
-        }
-
-        return new Promise<zone.ResponseValue>((resolve, reject) => {
+        return new Promise<zone.ExecuteResult>((resolve, reject) => {
             this._nativeZone.execute(request, (response) => {
                 if (response.code === 0) {
                     transportContext = transport.createTransportContext(response.contextHandle);
-                    resolve(transport.unmarshall(response.returnValue, transportContext));
+                    resolve(new NapaExecuteResult(response.returnValue, transportContext));
                 } else {
                     reject(response.errorMessage);
                 }
-            }, timeout);
+            });
         });
     }
 
