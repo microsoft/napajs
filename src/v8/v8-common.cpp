70831b61e21a7e99bead35da88d88f77fd1be76e
Asi Bross <asibross@gmail.com>
2017-06-19 22:16:49 +0000

    Merged PR 300167: Support building napa with cmake
    Support building napa with cmake

--- src/v8/v8-common.cpp
+++ src/v8/v8-common.cpp
@@ -1,5 +1,13 @@
 #include "v8-common.h"
 
+#ifdef USING_V8_SHARED
+
+// Empty stubs when using shared v8
+bool napa::v8_common::Initialize() { return true; }
+void napa::v8_common::Shutdown() {}
+
+#else
+
 #include <napa-log.h>
 
 #include <boost/dll/runtime_symbol_info.hpp>
@@ -9,6 +17,7 @@
 #include <v8.h>
 
 static v8::Platform* _platform = nullptr;
+
 
 bool napa::v8_common::Initialize() {
     NAPA_ASSERT(!_platform, "V8 was already initialized");
@@ -29,3 +38,4 @@ void napa::v8_common::Shutdown() {
     delete _platform;
     _platform = nullptr;
 }
+#endif
