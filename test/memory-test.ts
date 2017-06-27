7dcc426893e369957aba75a9658e6d05aac5d1b4
Asi Bross <asib@microsoft.com>
2017-06-27 16:59:24 -0700

    Restructure tests folder and enable npm test

---
+++ test/memory-test.ts
@@ -0,0 +1,65 @@
+import * as napa from "..";
+import * as assert from 'assert';
+import * as path from 'path';
+
+let NAPA_ZONE_TEST_MODULE = path.resolve(__dirname, 'napa-zone/test');
+
+describe('napajs/memory', () => {
+    let napaZone = napa.zone.create('zone5');
+
+    describe('Handle', () => {
+        it('#isEmpty', () => {
+            assert(napa.memory.isEmpty([0, 0]));
+            assert(!napa.memory.isEmpty([1, 2]));
+        });
+    });
+
+    describe('Shareable', () => {
+        it('#isShareable', () => {
+            assert(napa.memory.isShareable(napa.memory.crtAllocator));
+            assert(!napa.memory.isShareable("hello world"));
+            assert(!napa.memory.isShareable(1));
+            assert(!napa.memory.isShareable({ a: 1 }));
+        });
+    });
+
+    describe('Allocators', () => {
+        it('@node: crtAllocator', () => {
+            let handle = napa.memory.crtAllocator.allocate(10);
+            assert(!napa.memory.isEmpty(handle));
+            napa.memory.crtAllocator.deallocate(handle, 10);
+        });
+        
+        it('@napa: crtAllocator', () => {
+            napaZone.executeSync(NAPA_ZONE_TEST_MODULE, "crtAllocatorTest", []);
+        });
+    
+        it('@node: defaultAllocator', () => {
+            let handle = napa.memory.defaultAllocator.allocate(10);
+            assert(!napa.memory.isEmpty(handle));
+            napa.memory.defaultAllocator.deallocate(handle, 10);
+        });
+
+        it('@napa: defaultAllocator', () => {
+            napaZone.executeSync(NAPA_ZONE_TEST_MODULE, "defaultAllocatorTest", []);
+        });
+
+        it('@node: debugAllocator', () => {
+            let allocator = napa.memory.debugAllocator(napa.memory.defaultAllocator);
+            let handle = allocator.allocate(10);
+            assert(!napa.memory.isEmpty(handle));
+            allocator.deallocate(handle, 10);
+            let debugInfo = JSON.parse(allocator.getDebugInfo());
+            assert.deepEqual(debugInfo, {
+                allocate: 1,
+                allocatedSize: 10,
+                deallocate: 1,
+                deallocatedSize: 10
+            });
+        });
+
+        it('@napa: debugAllocator', () => {
+            napaZone.executeSync(NAPA_ZONE_TEST_MODULE, "debugAllocatorTest", []);
+        });
+    });
+});
---
+++ test/napa-zone/test.ts
@@ -0,0 +1,212 @@
+import * as napa from '../..';
+import * as assert from 'assert';
+
+export function bar(input: any) {
+    return input;
+}
+
+export namespace ns1 {
+    export namespace ns2 {
+        export function foo(input: any) {
+            return input;
+        }
+    }
+}
+
+export function getCurrentZone(): napa.zone.Zone {
+    return napa.zone.current;
+}
+
+export function broadcast(id: string, code: string): void {
+    let zone = napa.zone.get(id);
+    // TODO: replace with broadcast when TODO:#3 is done.
+    zone.broadcastSync(code);
+}
+
+export function broadcastTestFunction(id: string): void {
+    // TODO: replace with broadcast when TODO:#3 is done.
+    napa.zone.get(id).broadcastSync((input: string) => {
+        console.log(input);
+    }, ["hello world"]);
+}
+
+export function broadcastTransportable(id: string): void {
+    // TODO: replace with broadcast when TODO:#3 is done.
+    napa.zone.get(id).broadcastSync((input: any) => {
+        console.log(input);
+    }, [napa.memory.crtAllocator]);
+}
+
+export function broadcastClosure(id: string): void {
+    let zone = napa.zone.get(id);
+    // TODO: replace with broadcast when TODO:#3 is done.
+    zone.broadcastSync(() => {
+        console.log(zone);
+    }, []);
+}
+
+export function execute(id: string, moduleName: string, functionName: string, args?: any[]): any {
+    let zone = napa.zone.get(id);
+    // TODO: replace with execute when TODO:#3 is done.
+    return zone.executeSync(moduleName, functionName, args).value;
+}
+
+export function executeTestFunction(id: string): any {
+    let zone = napa.zone.get(id);
+    // TODO: replace with execute when TODO:#3 is done.
+    return zone.executeSync((input: string) => {
+            return input;
+        }, ['hello world']).value;
+}
+
+export function executeTestFunctionWithClosure(id: string): any {
+    let zone = napa.zone.get(id);
+    // TODO: replace with execute when TODO:#3 is done.
+    return zone.executeSync(() => { return zone; }, []).value;
+}
+
+/// <summary> Memory test helpers. </summary>
+export function crtAllocatorTest(): void {
+    let handle = napa.memory.crtAllocator.allocate(10);
+    assert(!napa.memory.isEmpty(handle));
+    napa.memory.crtAllocator.deallocate(handle, 10);
+}
+
+export function defaultAllocatorTest(): void {
+    let handle = napa.memory.defaultAllocator.allocate(10);
+    assert(!napa.memory.isEmpty(handle));
+    napa.memory.defaultAllocator.deallocate(handle, 10);
+}
+
+export function debugAllocatorTest(): void {
+    let allocator = napa.memory.debugAllocator(napa.memory.defaultAllocator);
+    let handle = allocator.allocate(10);
+    assert(!napa.memory.isEmpty(handle));
+    allocator.deallocate(handle, 10);
+    let debugInfo = JSON.parse(allocator.getDebugInfo());
+    assert.deepEqual(debugInfo, {
+            allocate: 1,
+            allocatedSize: 10,
+            deallocate: 1,
+            deallocatedSize: 10
+        });
+}
+
+let store2: napa.store.Store = null;
+export function getOrCreateStoreTest() {
+    store2 = napa.store.getOrCreate('store2');
+    assert(store2 != null);
+    assert.equal(store2.id, 'store2');
+    assert.equal(store2.size, 0);
+}
+
+export function getStoreTest() {
+    let store = napa.store.get('store2');
+    assert.equal(store.id, 'store2');
+
+    // Store created from node zone.
+    let store1 = napa.store.get('store1');
+    assert.equal(store1.id, 'store1');
+}
+
+export function storeVerifyGet(storeId: string, key: string, expectedValue: any) {
+    let store = napa.store.get(storeId);
+    assert.deepEqual(store.get(key), expectedValue);
+}
+
+export function storeGetCompareHandle(storeId: string, key: string, expectedHandle: napa.memory.Handle) {
+    let store = napa.store.get(storeId);
+    assert.deepEqual(store.get(key).handle, expectedHandle);
+}
+
+export function storeSet(storeId: string, key: string, expectedValue: any) {
+    let store = napa.store.get(storeId);
+    store.set(key, expectedValue);
+}
+
+export function storeDelete(storeId: string, key: string) {
+    let store = napa.store.get(storeId);
+    store.delete(key);
+}
+
+export function storeVerifyNotExist(storeId: string, key: string) {
+    let store = napa.store.get(storeId);
+    assert(!store.has(key));
+    assert(store.get(key) === undefined);
+}
+
+/// <summary> Transport test helpers. </summary>
+export class CannotPass {
+    field1: string;
+    field2: number;
+}
+
+@napa.transport.cid(module.id)
+export class CanPass extends napa.transport.TransportableObject {
+    constructor(allocator: napa.memory.Allocator) {
+        super();
+        this._allocator = allocator;
+    }
+    
+    save(payload: any, tc: napa.transport.TransportContext): void {
+        payload['_allocator'] = this._allocator.marshall(tc) 
+    }
+
+    load(payload: any, tc: napa.transport.TransportContext): void {
+        // Do nothing, as all members are transportable, which will be restored automatically.
+    }
+
+    _allocator: napa.memory.Allocator;
+}
+
+function testMarshallUnmarshall(input: any) {
+    let tc = napa.transport.createTransportContext();
+    let payload = napa.transport.marshall(input, tc);
+    let expected = napa.transport.unmarshall(payload, tc);
+    assert.equal(input.toString(), expected.toString());
+}
+
+export function simpleTypeTransportTest() {
+    testMarshallUnmarshall({ 
+        a: 'hello',
+        b: {
+            c: [0, 1]
+        }
+    });
+}
+
+export function jsTransportTest() {
+    testMarshallUnmarshall(new CanPass(napa.memory.crtAllocator));
+}
+
+export function functionTransportTest() {
+    testMarshallUnmarshall(() => { return 0; });
+}
+
+export function addonTransportTest() {
+    testMarshallUnmarshall(napa.memory.debugAllocator(napa.memory.crtAllocator));
+}
+
+export function compositeTransportTest() {
+    testMarshallUnmarshall({
+        a: napa.memory.debugAllocator(napa.memory.crtAllocator),
+        b: [1, 2],
+        c: {
+            d: napa.memory.defaultAllocator,
+            e: 1
+        }
+    });
+}
+
+export function nontransportableTest() {
+    let tc = napa.transport.createTransportContext();
+    let input = new CannotPass();
+    let success = false;
+    try {
+        napa.transport.marshall(input, tc);
+        success = true;
+    }
+    catch(error) {
+    }
+    assert(!success);
+}
