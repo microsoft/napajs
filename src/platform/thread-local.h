b51925cfed4f2588a332f6c0141004fd923da372
Yulong Wang <f.s@qq.com>
2017-07-20 16:47:05 -0700

    Fix build warnings (#2)
    * Fix build warnings
    
    * fix build break
    
    * Fix build break in unittest

--- src/platform/thread-local.h
+++ src/platform/thread-local.h
@@ -1,6 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license.
-#pragma once;
+#pragma once
 
 #include <napa/assert.h>
 #include <platform/platform.h>
--- unittest/settings/parser-tests.cpp
+++ unittest/settings/parser-tests.cpp
@@ -28,9 +28,9 @@ TEST_CASE("Parsing from console", "[settings-parser]") {
     settings::PlatformSettings settings;
     settings.loggingProvider = "";
 
-    std::vector<char*> args = { "--loggingProvider", "myProvider" };
+    std::vector<const char*> args = { "--loggingProvider", "myProvider" };
 
-    REQUIRE(settings::ParseFromConsole(static_cast<int>(args.size()), args.data(), settings));
+    REQUIRE(settings::ParseFromConsole(static_cast<int>(args.size()), const_cast<char**>(args.data()), settings));
     REQUIRE(settings.loggingProvider == "myProvider");
 }
 
