5034af24c39fc55add90a5dac865f20496c051ef
Daiyi Peng <dapeng@microsoft.com>
2017-07-31 22:21:30 -0700

    Fix marshall/unmarshall with latest transport context change.

--- inc/napa/transport/transport.h
+++ inc/napa/transport/transport.h
@@ -14,9 +14,8 @@ namespace transport {
     /// <param name="constructor"> Constructor of wrap type. </param>
     /// <remarks> 'napajs/lib/transport/transport' is required instead of 'napajs/lib/transport' to avoid circular dependency on addon. </remarks>
     inline void Register(v8::Local<v8::Function> constructor) {
-        constexpr int argc = 1;
-        v8::Local<v8::Value> argv[argc] = { constructor };
-        (void)napa::module::binding::Call("../lib/transport/transport", "register", argc, argv);
+        v8::Local<v8::Value> argv[] = { constructor };
+        (void)napa::module::binding::Call("../lib/transport/transport", "register", sizeof(argv) / sizeof(v8::Local<v8::Value>), argv);
     }
 
     /// <summary> Marshall an object with transport context. C++ modules can use this helper function to marshall its members. </summary>
@@ -25,9 +24,12 @@ namespace transport {
     /// <returns> Payload in V8 string of marshalled object. </summary>
     /// <remarks> 'napajs/lib/transport/transport' is required instead of 'napajs/lib/transport' to avoid circular dependency on addon. </remarks>
     inline v8::MaybeLocal<v8::String> Marshall(v8::Local<v8::Value> object, v8::Local<v8::Object> transportContextWrap) {
-        constexpr int argc = 2;
-        v8::Local<v8::Value> argv[argc] = { object, transportContextWrap };
-        return v8_helpers::MaybeCast<v8::String>(napa::module::binding::Call("../lib/transport/transport", "marshall", argc, argv));
+        v8::Local<v8::Value> argv[] = { object, transportContextWrap };
+        return v8_helpers::MaybeCast<v8::String>(napa::module::binding::Call(
+            "../lib/transport/transport", 
+            "marshall", 
+            sizeof(argv) / sizeof(v8::Local<v8::Value>), 
+            argv));
     }
 
     /// <summary> Marshall an object with transport context. C++ modules can use this helper function to marshall its members. </summary>
@@ -36,9 +38,16 @@ namespace transport {
     /// <returns> Payload in V8 string of marshalled object. </summary>
     /// <remarks> 'napajs/lib/transport/transport' is required instead of 'napajs/lib/transport' to avoid circular dependency on addon. </remarks>
     inline v8::MaybeLocal<v8::String> Marshall(v8::Local<v8::Value> object, napa::transport::TransportContext* transportContext) {
-        constexpr int argc = 1;
-        v8::Local<v8::Value> argv[argc] = { v8_helpers::PtrToV8Uint32Array(v8::Isolate::GetCurrent(), transportContext) };
-        auto transportContextWrap = napa::module::binding::NewInstance("TransportContextWrap", argc, argv).ToLocalChecked();
+        auto isolate = v8::Isolate::GetCurrent();
+        v8::Local<v8::Value> argv[] = { 
+            v8::Boolean::New(isolate, false),                      // Not owning.
+            v8_helpers::PtrToV8Uint32Array(isolate, transportContext) 
+        };
+        auto transportContextWrap = napa::module::binding::NewInstance(
+            "TransportContextWrap", 
+            sizeof(argv) / sizeof(v8::Local<v8::Value>), 
+            argv).ToLocalChecked();
+
         return Marshall(object, transportContextWrap);
     }
 
@@ -48,9 +57,8 @@ namespace transport {
     /// <returns> Unmarshalled V8 value from payload. </summary>
     /// <remarks> 'napajs/lib/transport/transport' is required instead of 'napajs/lib/transport' to avoid circular dependency on addon. </remarks>
     inline v8::MaybeLocal<v8::Value> Unmarshall(v8::Local<v8::Value> payload, v8::Local<v8::Object> transportContextWrap) {
-        constexpr int argc = 2;
-        v8::Local<v8::Value> argv[argc] = { payload, transportContextWrap };
-        return napa::module::binding::Call("../lib/transport/transport", "unmarshall", argc, argv);
+        v8::Local<v8::Value> argv[] = { payload, transportContextWrap };
+        return napa::module::binding::Call("../lib/transport/transport", "unmarshall", sizeof(argv) / sizeof(v8::Local<v8::Value>), argv);
     }
 
     /// <summary> Unmarshall a payload with transport context. C++ modules can use this helper function to unmarshall its members. </summary>
@@ -59,9 +67,16 @@ namespace transport {
     /// <returns> Unmarshalled V8 value from payload. </summary>
     /// <remarks> 'napajs/lib/transport/transport' is required instead of 'napajs/lib/transport' to avoid circular dependency on addon. </remarks>
     inline v8::MaybeLocal<v8::Value> Unmarshall(v8::Local<v8::Value> payload, const napa::transport::TransportContext* transportContext) {
-        constexpr int argc = 1;
-        v8::Local<v8::Value> argv[argc] = { v8_helpers::PtrToV8Uint32Array(v8::Isolate::GetCurrent(), transportContext) };
-        auto transportContextWrap = napa::module::binding::NewInstance("TransportContextWrap", argc, argv).ToLocalChecked();
+        auto isolate = v8::Isolate::GetCurrent();
+        v8::Local<v8::Value> argv[] = { 
+            v8::Boolean::New(isolate, false),
+            v8_helpers::PtrToV8Uint32Array(isolate, transportContext) 
+        };
+        auto transportContextWrap = napa::module::binding::NewInstance(
+            "TransportContextWrap", 
+            sizeof(argv) / sizeof(v8::Local<v8::Value>), 
+            argv).ToLocalChecked();
+
         return Unmarshall(payload, transportContextWrap);
     }
 }
