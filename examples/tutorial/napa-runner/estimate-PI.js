3bb7c922872aab9db02e8f11eb33573d587f5ca8
Allen Wang <helloshuangzi@gmail.com>
2017-11-08 11:14:18 -0800

    napa embeded support for Linux (#110)
    * napa embeded building for linux
    
    * polish building solution and readme.md
    
    * napa runner
    
    * add License
    
    * change dir to napa-runner
    
    * fix comments
    
    * fix comments and add TODOs
    
    * fix typo

---
+++ examples/tutorial/napa-runner/estimate-PI.js
@@ -0,0 +1,42 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+
+function estimatePI(points) {
+    var i = points;
+    var inside = 0;
+    while (i-- > 0) {
+        if (i%10000 == 0) console.log(i);
+        var x = Math.random();
+        var y = Math.random();
+        if ((x * x) + (y * y) <= 1) {
+            inside++;
+        }
+    }
+        return inside / points * 4;
+}
+
+function run() {
+    console.log('start run');
+
+    var napa = require('napajs');
+    console.log('napajs loaded');
+    const NUMBER_OF_WORKERS = 4;
+    var zone = napa.zone.create('zone1', { workers: NUMBER_OF_WORKERS });
+    console.log('zone created with 4 workers');
+
+    var promises = [];
+    for (var i = 0; i < 4; i++) {
+        promises[i] = zone.execute(estimatePI, [40000]);
+    }
+
+    console.log('4 times napa.zone.execution issued.');
+    Promise.all(promises).then(values => {
+        var aggregate = 0;
+        values.forEach(result => aggregate += result.value);
+        console.log('PI: ', aggregate / 4);
+    });
+
+    return 'returned-value-to-cpp-world';
+}
+
+run();
