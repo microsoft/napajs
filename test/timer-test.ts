e847e1ef70ade41e28804b669c2a4696c69a360d
Lei Zhang <zhang.huanning@hotmail.com>
2018-02-28 12:57:55 -0800

    add test case for napa set/clear Immediate/Timeout/Interval.

--- test/timer-test.ts
+++ test/timer-test.ts
@@ -4,145 +4,207 @@
 // To Run this test, in napajs root directory after build, use:
 //      node test/timer-test 
 
-import * as assert from "assert";
-import * as path from "path";
 import * as napa from "../lib/index";
 
+
 // To be execute in napa workers
-export function setImmediateTest(taskGroupId: string) : Promise<string> {
-    const kTaskGroupSize = 3;
-    //TODO: where to make it global?
+export async function setImmediateTest(taskGroupId: number) : Promise<string> {
+    const kTaskGroupSize = 5;
+    const kAllowedScheduleDiffInMS = 20;
+
     let setImmediate = napa.timer.setImmediate;
     let clearImmediate = napa.timer.clearImmediate;
 
     let correctResult = "";
-    let lastTaskString = "";
+    let lastTaskId = 0;
     for (let taskId = 0; taskId < kTaskGroupSize; taskId++) {
-        let taskString = "" + taskGroupId + "-" + taskId;
         if (taskId != 1) {
-            correctResult = correctResult + taskString + " : ";
-            lastTaskString = taskString;
+            correctResult = `${correctResult}:${taskId}_OnTime`;
+            lastTaskId = taskId;
         }
     }
 
-
-    var promise = new Promise<string>((resolve, reject) => {
+    let promise = new Promise<string>((resolve, reject) => {
         let execResult = "";
         for (let taskId = 0; taskId < kTaskGroupSize; taskId++) {
-            let taskString = "" + taskGroupId + "-" + taskId;
-            console.log("  ++Task:" +  taskString + " setting immediate call back");
-            let immedidate = setImmediate((taskString: string) => {
-                console.log("    Running Task:" + taskString + " in set immedidate call back");
-                execResult = execResult + taskString + " : ";
-                if (taskString == lastTaskString) {
+            let startTime = Date.now();
+            let immedidate = setImmediate((lastTaskId: number) => {
+                let delayToRun = Date.now() - startTime;
+                execResult = `${execResult}:${taskId}_OnTime`;
+                if (delayToRun > kAllowedScheduleDiffInMS) {
+                    execResult = `${execResult}(X)`;
+                }
+                if (taskId == lastTaskId) {
                     if (execResult == correctResult) {
-                        resolve(execResult)
+                        resolve(`OK:${execResult}`)
                     }
                     else {
-                        reject(execResult)
+                        reject(`FAIL:${execResult} vs ${correctResult}`)
                     }
                 }
-            }, [taskString]);
-            console.log("  ++Task:" + taskString + " setted immediate call back");
+            }, lastTaskId);
     
             if (taskId == 1) {
-                console.log("  --Task:" + taskString + " clearing immediate call back");
                 clearImmediate(immedidate);
-                console.log("  --Task:" + taskString + " cleared immediate call back");
             }
         }
     });
+
     return promise;
 }
 
-export function setTimeoutTest(taskGroupId: string) {
-    const kTaskGroupSize = 3;
-    //TODO: where to make it global?
+export function setTimeoutTest(taskGroupId: number) : Promise<string> {
+    const kTaskGroupSize = 5;
+    const kAllowedScheduleDiffInMS = 20;
+
     let setTimeout = napa.timer.setTimeout;
     let clearTimeout = napa.timer.clearTimeout;
 
+    let correctResult = "";
+    let lastTaskId = 0;
     for (let taskId = 0; taskId < kTaskGroupSize; taskId++) {
-        let taskString = "" + taskGroupId + "-" + taskId;
-        let wait = 100 * ((+taskGroupId) * kTaskGroupSize + taskId);
-        console.log("  ++Task:" +  taskString + " setting timeout call back");
-        let timeout = setTimeout((taskString: string) => {
-            console.log("    Running Task:" + taskString + " in set timeout call back");
-        }, wait, [taskString]);
-        console.log("  ++Task:" + taskString + " setted timeout call back");
-
-        if (taskId == 1) {
-            console.log("  --Task:" + taskString + " clearing timeout call back");
-            clearTimeout(timeout);
-            console.log("  --Task:" + taskString + " cleared timeout call back");
+        if (taskId != 1) {
+            correctResult = `${correctResult}:${taskId}_OnTime`;
+            lastTaskId = taskId;
         }
     }
+    
+    let promise = new Promise<string>((resolve, reject) => {
+        let execResult = "";
+        for (let taskId = 0; taskId < kTaskGroupSize; taskId++) {
+            let wait = 200 * (taskGroupId * kTaskGroupSize + taskId + 1);
+            let startTime = Date.now();
+            let timeout = setTimeout((lastTaskId: number) => {
+                let waitToRun = Date.now() - startTime;
+                execResult = `${execResult}:${taskId}_OnTime`;
+                if (Math.abs(waitToRun - wait) > kAllowedScheduleDiffInMS) {
+                    execResult = `${execResult}(X)`;
+                }
+
+                if (taskId == lastTaskId) {
+                    if (execResult == correctResult) {
+                        resolve(`OK:${execResult}`)
+                    }
+                    else {
+                        reject(`FAIL:${execResult} .vs. ${correctResult}`)
+                    }
+                }
+            }, wait, lastTaskId);
+
+            if (taskId == 1) {
+                clearTimeout(timeout);
+            }
+        }
+    });
+
+    return promise;
 }
 
 
-export function setIntervalTest(taskGroupId: string, duration: number, count: number) {
-    //TODO: where to make it global?
+export function setIntervalTest(taskGroupId: number, duration: number, count: number) : Promise<string> {
+    const kAllowedScheduleDiffInMS = 20;
+
     let setInterval = napa.timer.setInterval;
     let clearInterval = napa.timer.clearInterval;
     let setTimeout = napa.timer.setTimeout;
     let clearTimeout = napa.timer.clearTimeout;
 
-    console.log("  Interval Task:" + taskGroupId + " trying to run every " + duration + "ms for " + count + " times...");
+    let correctResult = "";
+    for (let i = 0; i < count; ++i) {
+        correctResult = `${correctResult}:${i}_OnTime`
+    }
 
-    let interval = setInterval((taskString: string) => {
-        console.log("    ---- Task:" + taskString + " in set interval call back");
-    }, duration, [taskGroupId])
+    let repeatCount = 0;
+    let execResult = "";
+    let startTime = Date.now();
+    let interval = setInterval((taskGroupId: number, duration: number) => {
+        let wait = Date.now() - startTime;
+        execResult = `${execResult}:${repeatCount}_OnTime`;
+        let avgScheduleDiff = Math.abs(wait - (1 + repeatCount) * duration) / (1 + repeatCount);
+        if (avgScheduleDiff > kAllowedScheduleDiffInMS) {
+            execResult = `${execResult}(X)`;
+        }
+        repeatCount = repeatCount + 1;
+    }, duration, taskGroupId, duration)
 
-    setTimeout((taskString: String)=> { 
-        console.log("    ---- Task:" + taskString + " clearing interval...");
+    setTimeout(()=> { 
         clearInterval(interval);
-    }, duration * count, [taskGroupId]);
+    }, duration * (count + 0.5));
+
+    let promise = new Promise<string>((resolve, reject) => {
+        setTimeout(() => {
+            if (execResult == correctResult) {
+                resolve(`OK:${execResult}`)
+            }
+            else {
+                reject(`FAIL:${execResult} .vs. ${correctResult}`)
+            }
+        }, duration * (count + 5));
+    });
+    return promise;
 }
 
+var zone: napa.zone.Zone;
+
+async function initZone(workerCount: number) {
+    zone = napa.zone.create('zone', { workers: workerCount });
+    await zone.broadcast('');
+}
 
 declare var __in_napa: boolean;
-
 if (typeof __in_napa === 'undefined') {
-    // Change this value to control number of napa workers initialized.
-    const NUMBER_OF_WORKERS = 2;
-    const kTaskGroupCount = 5;
-    let zone = napa.zone.create('zone', { workers: NUMBER_OF_WORKERS });
+    let assert = require('assert');
 
-    zone.broadcast('')
-        .then(null, (err)=>{
-            console.log("Error zone broadcast: " + err);
-        })
+    const NUMBER_OF_WORKERS = 3;
+    initZone(NUMBER_OF_WORKERS);
 
-    console.log("Waiting zone initalization......");
-    setTimeout(()=>{
-        console.log("\nStart setImmediate/clearImmediate test......");
+    const kTaskGroupCount = 3;
+
+    describe("SetImmediate/clearImmediate", function() {
+        let promises: Promise<napa.zone.Result>[] = [];
         for (let groupId = 0; groupId < kTaskGroupCount; groupId++) {
-            zone.execute('./timer-test', 'setImmediateTest', [groupId.toString()])
-            .then(
-                (res)=>{ console.log("\n\n=============\nCorrect result:" + res.toString()); },
-                (err) =>{ console.log("\n\n=============\nWrong result:"+ err.toString()); }
+            let res = zone.execute('./timer-test', 'setImmediateTest', [groupId]);
+            promises.push(res);
+        }
+
+        for (let groupId = 0; groupId < kTaskGroupCount; groupId++) {
+            it(`Immediate test group:${groupId} should return string prefixed with OK`, 
+                async function() {
+                    let result = (await promises[groupId]).value;
+                    assert(result.startsWith('OK'), `${result}`);
+                }
             );
         }
-    }, 300);
+    });
 
 
-    setTimeout(()=>{
-        console.log("\nStarting setTimeout/clearTimerout testing......");
+    describe("SetTimeout/clearTimeout", async function() {
+        let promises: Promise<napa.zone.Result>[] = [];
         for (let groupId = 0; groupId < kTaskGroupCount; groupId++) {
-            zone.execute('./timer-test', 'setTimeoutTest', [groupId.toString()]);
+            let res = zone.execute('./timer-test', 'setTimeoutTest', [groupId]);
+            promises.push(res);
         }
-    }, 1200);
 
-
-    setTimeout(()=>{
-        console.log("\nStarting setInterval/clearInterval testing......");
-        for (let groupId = 0; groupId < 2; groupId++) {
-            zone.execute('./timer-test', 'setIntervalTest', [groupId.toString(), 300*(groupId+1), 6]);
+        for (let groupId = 0; groupId < kTaskGroupCount; groupId++) {
+            it(`Timeout test group:${groupId} should return string prefixed with OK`, 
+                async function() {
+                    let result = (await promises[groupId]).value;
+                    assert(result.startsWith('OK'), `${result}`);
+                }
+            );
         }
-    }, 3200);
+    });
 
-    setTimeout(()=>{
-        console.log("\nFinished waiting all timer test run.");
-    }, 9900);
+
+    describe("setInterval/clearInterval", function() {
+        it(`Interval test should return string prefixed with OK`, 
+            async function() {
+                let promise = zone.execute('./timer-test', 'setIntervalTest', ["0", 200, 6]);
+                let result = (await promise).value;
+                assert(result.startsWith('OK'), `${result}`);
+            }
+        ).timeout(3000);
+    });
 }
 
 
