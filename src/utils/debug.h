9da92f0a30dc7d43fc78410767daecc879056c87
Yulong Wang <f.s@qq.com>
2018-03-01 14:29:48 -0800

    Update debug.h

--- src/utils/debug.h
+++ src/utils/debug.h
@@ -12,6 +12,6 @@
 #else
 
 // Do nothing without generating any "unreferenced variable" warnings.
-template <typename... Types> inline void LOG_DEBUG(Types&&...) {}
+template <typename... Types> inline void NAPA_DEBUG(Types&&...) {}
 
 #endif
