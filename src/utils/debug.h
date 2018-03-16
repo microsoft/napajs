b59817dc8bb4c3912cb5aedf0f9f0fb6e90461f9
Lei Zhang <zhang.huanning@hotmail.com>
2018-03-16 14:46:00 -0700

    correct building scripts for DEBUG_NAPA macro

--- src/utils/debug.h
+++ src/utils/debug.h
@@ -4,9 +4,10 @@
 #pragma once
 
 #include "napa/log.h"
-#define DEBUG_NAPA 1
+
+// use cmake-js -CDDEBUG_NAPA to define DEBUG_NAPA when compiling
 #if defined(DEBUG_NAPA)
     #define NAPA_DEBUG(section, format, ...) LOG_DEBUG(section, format, ##__VA_ARGS__)
 #else
-    #define NAPA_DEBUG
+    #define NAPA_DEBUG(section, format, ...) ((void)0)
 #endif
